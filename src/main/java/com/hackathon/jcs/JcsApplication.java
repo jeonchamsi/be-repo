cb792f512be765a8f980b38f000efdc614d05375
sjmoon00 <paulmoon00@naver.com>
2025-08-02 17:42:58 +0900

    init

---
+++ src/main/java/com/hackathon/jcs/JcsApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.jcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackathon/jcs/JcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.jcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
